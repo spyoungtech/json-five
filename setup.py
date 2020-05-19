93b0326a0d06caeba215165d88e0881f8b177552
Spencer Young <spencer.young@spyoung.com>
2020-05-18 17:54:15 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='json-five',
-    version='0.0.2',
+    version='0.0.3',
     packages=['json5'],
     url='https://github.com/spyoungtech/json-five',
     license='Apache',
